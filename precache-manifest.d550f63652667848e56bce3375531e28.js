75ba4268b1cf71bb208c27db21c4c92f44de388c
Zhitian Guan <zguan.university@gmail.com>
2021-01-01 01:57:51 -0500

    Updates

---
+++ precache-manifest.d550f63652667848e56bce3375531e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd55bfd1f7335ef5cd54f8ad55377ab4",
+    "url": "/React-Podcast-App/index.html"
+  },
+  {
+    "revision": "baa5b4da5739853ab8b6",
+    "url": "/React-Podcast-App/static/css/main.65842c79.chunk.css"
+  },
+  {
+    "revision": "5986656a023f04176622",
+    "url": "/React-Podcast-App/static/js/2.e96f2d6f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/React-Podcast-App/static/js/2.e96f2d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa5b4da5739853ab8b6",
+    "url": "/React-Podcast-App/static/js/main.0f889752.chunk.js"
+  },
+  {
+    "revision": "3b61900d0e73d4b4774e",
+    "url": "/React-Podcast-App/static/js/runtime-main.2919bc4e.js"
+  }
+]);
